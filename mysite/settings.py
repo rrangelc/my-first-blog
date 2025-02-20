779f676aeb32b2fd1abcbfba3801b4badb107f11
rrangel <rrangelc@gmail.com>
2025-02-19 23:49:33 -0500

    Tus cambios locales2

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,35 +73,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-<<<<<<< HEAD
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'homestead',
-#        'USER': '',
-#        'PASSWORD': '',
-#        'HOST': 'localhost',  # O la dirección IP de tu servidor
-##        'PORT': '3306',      # Puerto por defecto de MariaDB
-#        'OPTIONS': {
-#            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#        }
-#    }
-#}
-=======
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'homestead',
-#        'USER': 'root',
-#         'PASSWORD': 'r00t',
-#         'HOST': 'localhost',  # O la dirección IP de tu servidor
-#        'PORT': '3306',      # Puerto por defecto de MariaDB
-#        'OPTIONS': {
-#            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#         }
-#    }
-# }
 # f2fbeef (SQLite)
 
 DATABASES = {
